45ba29b52281bfa82c6073392a82064954dce322
muhammad-asher <70090506+muhammad-asher@users.noreply.github.com>
2020-09-06 16:34:23 +0500

    Added some wireframe-UI functionality

--- src/App.js
+++ src/App.js
@@ -1,26 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import { MenuItem, FormControl, Select } from "@material-ui/core";
+
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [countries, setcountries] = useState([]);
+	const [country, setcountry] = useState("Worldwide");
+
+	useEffect(() => {
+		const getcountriesdata = async () => {
+			await fetch("https://disease.sh/v3/covid-19/countries")
+				.then((response) => response.json())
+				.then((data) => {
+					const countries = data.map((country) => ({
+						name: country.country,
+						value: country.countryInfo.iso2,
+					}));
+					setcountries(countries);
+				});
+		};
+		getcountriesdata();
+	}, []);
+
+	const onCountryChange = async (e) => {
+		const countrycode = e.target.value;
+		setcountry(countrycode);
+	};
+	return (
+		<div className="app">
+			<div className="app-header">
+				<h1>COVID-19 TRACKER</h1>
+				<FormControl className="app-dropdown"></FormControl>
+				<Select variant="outlined" onChange={onCountryChange} value={country}>
+					<MenuItem variant="outlined" value="Worldwide">
+						Worldwide
+					</MenuItem>
+					{countries.map((country) => (
+						<MenuItem value={country.value}>{country.name}</MenuItem>
+					))}
+				</Select>
+			</div>
+		</div>
+	);
 }
 
 export default App;
