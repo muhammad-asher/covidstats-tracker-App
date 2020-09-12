d50fccfb29e6761b0c2e26c5d37b43c008451e1e
muhammad-asher <70090506+muhammad-asher@users.noreply.github.com>
2020-09-12 10:28:46 +0500

    Update Map.js

--- src/Map.js
+++ src/Map.js
@@ -9,7 +9,7 @@ function Map({ countries, casesType, center, zoom }) {
 			<LeafletMap center={center} zoom={zoom}>
 				<TileLayer
 					url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-					attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+					attribution='&copy; <a href="https://github.com/muhammad-asher">Github Link</a>'
 				/>
 				{showDataOnMap(countries, casesType)}
 			</LeafletMap>
